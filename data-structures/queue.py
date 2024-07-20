803c8f7c008b9a02c2e1e886e436d53d1d3dc2e9
Gregor Remec <remecgregor@gmail.com>
2024-07-19 20:26:44 -0600

    started on data structures

---
+++ data-structures/queue.py
@@ -0,0 +1,25 @@
+
+class Queue():
+
+    def __init__(self):
+        self.internal_list = []
+
+    def __init__(self, queue):
+        self.internal_list = [queue]
+
+    def push(self, item):
+        """Adds item to the back of queue."""
+        self.internal_list.insert(0, item)
+
+
+    def pop(self):
+        """Removes and returns item at the front of queue."""
+        if self.internal_list == []:
+            return None
+
+        pos = len(self.internal_list) - 1        
+        toReturn = self.internal_list[pos]
+        self.internal_list = self.internal_list[:pos]
+
+        return toReturn
+
