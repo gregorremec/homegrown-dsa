4d46cd973fcb13777ec94e1e2738b9dfddbf7959
Gregor Remec <remecgregor@gmail.com>
2024-07-23 18:39:00 -0600

    prime number generator added

---
+++ algorithms/math/prime-generator.py
@@ -0,0 +1,23 @@
+
+def generate_primes(start, end):
+    """Generates prime numbers between start and end values."""
+
+    primes = []
+
+    for i in range(start, end):
+        isPrime = True
+        if i < 2:
+            continue
+        else:
+            for j in range(2, int(i/2)+1):
+                if i % j == 0:
+                    isPrime = False
+                    break
+            if isPrime:
+                primes.append(i)
+
+    
+    return primes
+
+if __name__ == "__main__":
+    print(generate_primes(0, 15))
