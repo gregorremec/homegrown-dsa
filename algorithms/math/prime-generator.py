0f41875f269b0463303b6bb2804b0a6d86237182
Gregor Remec <remecgregor@gmail.com>
2024-07-24 20:00:10 -0600

    made sieve

--- algorithms/math/prime-generator.py
+++ algorithms/math/prime-generator.py
@@ -19,7 +19,34 @@ def generate_primes(start, end):
     
     return primes
 
-#TODO seive of eratosthenis
+
+def sieve_of_erato(num):
+    
+    primes = [True for _ in range(num + 1)]
+
+    toRet = []
+
+    p = 2
+
+    while p * p <= num:
+
+        if primes[p] is True:
+
+            for i in range(p*p, num+1, p):
+
+                primes[i] = False
+
+        p+=1
+    
+    for i in range(1, num+1):
+        if primes[i] is True:
+            toRet.append(i)
+
+    return toRet
+
+
+
 
 if __name__ == "__main__":
     print(generate_primes(0, 15))
+    print(sieve_of_erato(15))
